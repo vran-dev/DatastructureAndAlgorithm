33697cc5931a23d51462e4c4daea8922952f288c
keeperv <vran@vip.qq.com>
2017-07-22 00:17:14 +0800

    add graph imp

--- src/main/java/cc/cc1234/datastructure/graph/DenseGraph.java
+++ src/main/java/cc/cc1234/datastructure/graph/DenseGraph.java
@@ -5,27 +5,93 @@ package cc.cc1234.datastructure.graph;
  * 稠密图：使用邻接矩阵实现
  * @author vran
  */
-public class DenseGraph {
-	private int[][]  matrix;
+public class DenseGraph implements Graph{
+	private boolean[][]  matrix;
 	// 有向性
 	private boolean direct;
 	
+	private int vertexs;
+	private int edges;
+	
 	public DenseGraph(int capacity) {
-		matrix = new int[capacity][capacity];
-		this.direct = false;
+		this(capacity,false);
 	}
 	
 	public DenseGraph(int capacity, boolean direct) {
-		matrix = new int[capacity][capacity];
+		vertexs = capacity; 
+		matrix = new boolean[capacity][capacity];
 		this.direct = direct;
 	}
 	
-	public boolean addEdge(int x, int y) {
+	public boolean addEdge(int v, int w) {
+		if(!hasEdge(v, w)) {
+			matrix[v][w]=true;
+			if(!direct) {
+				matrix[w][v]=true;
+			}
+			edges++;
+			return true;
+		}
 		return false;
 	}
 	
-	public boolean containsEdge(int x, int y) {
-		return false;
+	public int vertexs() {
+		return vertexs;
+	}
+	
+	public int edges() {
+		return edges;
+	}
+	
+	public boolean hasEdge(int v, int w) {
+		return matrix[v][w];
+	}
+
+	@Override
+	public GraphIterator iterator(int v) {
+		return new GraphIteratorImp(v);
+	}
+	
+	private class GraphIteratorImp implements GraphIterator{
+		private int v;
+		private int index;
+		
+		public GraphIteratorImp(int v) {
+			this.v = v;
+		}
+
+		@Override
+		public int begin() {
+			index = 0;
+			for(int i = 0 ; i < matrix[v].length ;i++) {
+				if(matrix[v][i]) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		@Override
+		public int next() {
+			for(int i = index ; i < matrix[v].length ;i++) {
+				if(matrix[v][i]) {
+					index = i+1;
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		@Override
+		public boolean end() {
+			for(int i = matrix[v].length-1; i>=0 ;i--) {
+				if(matrix[v][i]) {
+					return i == index;
+				}
+			}
+			return false;
+		}
+		
 	}
 	
 }
---
+++ src/main/java/cc/cc1234/datastructure/graph/SparseGraph.java
@@ -0,0 +1,91 @@
+package cc.cc1234.datastructure.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 稀疏图
+ * @author vran1
+ *
+ */
+public class SparseGraph implements Graph{
+	private List<List<Integer>> linked;
+	private int vertexs;
+	private int edges;
+	private boolean direct;
+	
+	public SparseGraph(int capacity) {
+		this(capacity,  false);
+	}
+
+	public SparseGraph(int capacity, boolean direct) {
+		this.direct = direct;
+		linked = new ArrayList<>(capacity);
+		for(int i=0; i < capacity ;i++) {
+			linked.add(new ArrayList<Integer>());
+		}
+	}
+	
+	public boolean addEdge(int v, int w) {
+		if(hasEdge(v, w)) {
+			return false;
+		}
+		linked.get(v).add(w);
+		if(v!=w && !direct) {
+			linked.get(w).add(v);
+		}
+		edges++;
+		return true;
+	}
+	
+	public boolean hasEdge(int v, int w) {
+		for(int i=0;i<linked.get(v).size();i++) {
+			if(linked.get(v).get(i)==w) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int vertexs() {
+		return vertexs;
+	}
+	
+	public int edges() {
+		return edges;
+	}
+
+	@Override
+	public GraphIterator iterator(int v) {
+		return new GraphIteratorImp(v);
+	}
+	
+	private class GraphIteratorImp implements GraphIterator{
+		private int v;
+		private int index;
+		
+		public GraphIteratorImp(int v) {
+			this.v = v;
+		}
+
+		@Override
+		public int begin() {
+			index = 0;
+			return linked.get(v).get(0);
+		}
+
+		@Override
+		public int next() {
+			if(index < linked.get(v).size()) {
+				return linked.get(v).get(index++);
+			}
+			return -1;
+		}
+
+		@Override
+		public boolean end() {
+			return index == linked.get(v).size();
+		}
+		
+	}
+}
