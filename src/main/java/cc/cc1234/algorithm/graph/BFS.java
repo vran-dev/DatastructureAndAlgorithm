e8d9e8ee22256a5038e0abe089d15238b4001b3d
vkeeper <vran@vip.qq.com>
2017-08-03 18:25:49 +0800

    bfs imp 80%

--- src/main/java/cc/cc1234/algorithm/graph/BFS.java
+++ src/main/java/cc/cc1234/algorithm/graph/BFS.java
@@ -1,6 +1,11 @@
 package cc.cc1234.algorithm.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import cc.cc1234.datastructure.graph.Graph;
+import cc.cc1234.datastructure.graph.GraphIterator;
+import cc.cc1234.datastructure.graph.SparseGraph;
 
 
 /**
@@ -11,13 +16,47 @@ import cc.cc1234.datastructure.graph.Graph;
  */
 public class BFS {
 	private Graph graph;
-
+	private boolean[] visited;
+	private Queue<Integer> queue;
+	
 	public BFS(Graph graph) {
 		this.graph = graph;
+		visited = new boolean[graph.vertexs()];
+		queue = new LinkedList<>();
 	}
 	
 	public void order() {
+		for(int i = 0; i < graph.vertexs(); i++) {
+			queue.add(i);
+			while(!queue.isEmpty()) {
+				Integer x = queue.poll();
+				if(!visited[x]) {
+					visited[x] = true;
+					GraphIterator iterator = graph.iterator(x);
+					System.out.println("x="+x);
+					int j;
+					while((j = iterator.next())!=-1 && !visited[j]) {
+						System.out.println("\r\n"+x+" add: "+j);
+						queue.add(j);
+					}
+				}
+			}
+		}
 		
+	}
+	
+	public static void main(String[] args) {
+		Graph graph = new SparseGraph(9);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(1, 4);
+		graph.addEdge(2, 3);
+		graph.addEdge(4, 3);
+		graph.addEdge(4, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(3, 6);
+		graph.addEdge(7, 8);
+		new BFS(graph).order();
 	}
 	
 }
