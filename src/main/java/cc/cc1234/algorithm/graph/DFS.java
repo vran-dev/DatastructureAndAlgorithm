235e117c528ce95dc9a4f55a608061038399303b
keeperv <vran@vip.qq.com>
2017-08-03 22:01:33 +0800

    add union find

--- src/main/java/cc/cc1234/algorithm/graph/DFS.java
+++ src/main/java/cc/cc1234/algorithm/graph/DFS.java
@@ -1,5 +1,8 @@
 package cc.cc1234.algorithm.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cc.cc1234.datastructure.graph.DenseGraph;
 import cc.cc1234.datastructure.graph.Graph;
 import cc.cc1234.datastructure.graph.GraphIterator;
@@ -11,6 +14,7 @@ import cc.cc1234.datastructure.graph.GraphIterator;
  *
  */
 public class DFS {
+	private int[] union;
 	private boolean[] visited;
 	private Graph graph;
 	private int count;
@@ -18,27 +22,29 @@ public class DFS {
 	public DFS(Graph graph) {
 		this.graph = graph;
 		visited = new boolean[graph.vertexs()];
+		union = new int[graph.vertexs()];
 	}
 
-	public void order() {
+	public List<Integer> order() {
+		List<Integer> list = new ArrayList<>();
 		for(int i = 0; i < graph.vertexs(); i++) {
 			if(!visited[i]) {
-				System.out.print(i+"->");
-				doOrder(i);
+				doOrder(i,list);
 				count++;
-				System.out.println("\r\n");
 			}
 		}
+		return list;
 	}
 	
-	private void doOrder(int v) {
+	private void doOrder(int v,List<Integer> list) {
+		list.add(v);
+		union[v] = count;
 		visited[v] = true;
 		GraphIterator iterator = graph.iterator(v);
 		int r;
 		while((r=iterator.next())!=-1) {
 			if(!visited[r]) {
-				System.out.print(r+"->");
-				doOrder(r);
+				doOrder(r,list);
 			}
 		}
 	}
@@ -50,14 +56,25 @@ public class DFS {
 		return count;
 	}
 	
+	/*
+	 * 判断两个顶点是不是连通的
+	 */
+	public boolean connected(int v, int w) {
+		assert v>=0 && v<graph.vertexs():"arguments 0=< v <= vertexs";
+		assert w>=0 && w<graph.vertexs():"arguments 0<= w <= vertexs";
+		
+		return union[v] == union[w];
+	}
 	public static void main(String[] args) {
-		Graph graph = new DenseGraph(5);
-		GraphIterator iter = graph.iterator(0);
+		Graph graph = new DenseGraph(6);
 		graph.addEdge(0, 2);
-		graph.addEdge(1, 3);
-		graph.addEdge(0, 4);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 4);
+		graph.addEdge(1, 5);
+		
 		DFS dfs = new DFS(graph);
-		dfs.order();
+		List<Integer> list = dfs.order();
+		System.out.println(list);
 		System.out.println("联通分量="+dfs.count);
 	}
 }
