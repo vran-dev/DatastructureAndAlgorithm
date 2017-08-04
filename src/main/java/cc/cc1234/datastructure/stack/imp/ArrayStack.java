05870da8d164a89e98ac97fe372e6d1aebecf384
vkeeper <vran@vip.qq.com>
2017-08-04 15:31:55 +0800

    delete main

--- src/main/java/cc/cc1234/datastructure/stack/imp/ArrayStack.java
+++ src/main/java/cc/cc1234/datastructure/stack/imp/ArrayStack.java
@@ -58,23 +58,4 @@ public class ArrayStack<E> implements IStack<E>{
 	public String toString() {
 		return "Stack [data=" + Arrays.toString(data) + ", size=" + size + "]";
 	}
-
-	public static void main(String[] args) {
-		ArrayStack<Integer> stack = new ArrayStack<>(10);
-		stack.push(1);
-		stack.push(2);
-		stack.push(3);
-		stack.push(4);
-		stack.push(5);
-		stack.push(6);
-		System.out.println(stack);
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack.pop());
-		System.out.println(stack);
-	}
 }
