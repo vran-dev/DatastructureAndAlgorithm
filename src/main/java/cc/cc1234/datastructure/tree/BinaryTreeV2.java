b48fb423bff706ff9daa3b6bc84b3cc1a1984f8e
keeperv <vran@vip.qq.com>
2017-08-05 20:45:22 +0800

    update remove()

--- src/main/java/cc/cc1234/datastructure/tree/BinaryTreeV2.java
+++ src/main/java/cc/cc1234/datastructure/tree/BinaryTreeV2.java
@@ -6,9 +6,9 @@ package cc.cc1234.datastructure.tree;
  * @date 2017-07-31 16:46:19
  */
 public class BinaryTreeV2<E extends Comparable<? super E>> extends BaseTree<E>{
-	
-	private Node<E> root;
-	
+
+	Node<E> root;
+
 	private static class Node<E>{
 		E e;
 		Node<E> left;
@@ -17,22 +17,30 @@ public class BinaryTreeV2<E extends Comparable<? super E>> extends BaseTree<E>{
 			super();
 			this.e = e;
 		}
+		
+		public Node(Node<E> node) {
+			this.e = node.e;
+			this.left = node.left;
+			this.right = node.right;
+		}
+		
 		@Override
 		public String toString() {
 			return " [e=" + e + ", left=" + left + ", right=" + right + "]";
 		}
 	}
-	
+
 	@Override
 	public void insert(E e) {
 		root = doInsert(root,e);
+		size++;
 	}
-	
+
 	private Node<E> doInsert(Node<E> node, E e){
 		if(node == null) {
 			return new Node<>(e);
 		}
-		
+
 		if(node.e.compareTo(e) > 0) {
 			node.left = doInsert(node.left,e);
 		}else {
@@ -58,31 +66,49 @@ public class BinaryTreeV2<E extends Comparable<? super E>> extends BaseTree<E>{
 
 	@Override
 	public E remove(E e) {
+		if(size == 0) {
+			return null;
+		}
 		root = doRemove(root,e);
 		return e;
 	}
-	
+
 	private Node<E> doRemove(Node<E> node,E e){
 		if(node == null || e == null) {
 			return null;
 		}
+
 		if(node.e.compareTo(e) == 0){
-			Node<E> successor = successor(node);
-			if(successor != null) {
-				successor.left = node.left;
-				successor.right = node.right;
+			if(node.left == null) {
+				Node<E> n = node.right;
+				node.right = null;
+				size--;
+				return n;
 			}
+
+			if(node.right == null) {
+				Node<E> n = node.left;
+				node.left = null;
+				size--;
+				return n;
+			}
+
+			Node<E> successor = new Node<>(successor(node.right));
+			successor.right = removeMin(node.right);
+			successor.left = node.left;
+			
 			return successor;
 		}else if(node.e.compareTo(e) == 1) {
 			node.left =  doRemove(node.left, e);
+			return node;
 		}else {
 			node.right = doRemove(node.right, e);
+			return node;
 		}
-		return node;
 	}
-	
+
 	/**
-	 * 寻找指定节点的后继
+	 * 返回指定节点的后继
 	 * @param node  指定节点
 	 * @return
 	 * @Date 2017年7月31日
@@ -92,33 +118,39 @@ public class BinaryTreeV2<E extends Comparable<? super E>> extends BaseTree<E>{
 			return null;
 		}
 		
-		if(node.left == null) {
-			return node.right;
+		if(node.left == null) { // 此时表示node为后继节点
+			return node;
+		}else {
+			return successor(node.left);
 		}
-		Node<E> pre = null;
-		while(node.left!=null) {
-			pre = node;
-			node = node.left;
-		}
-		pre.left = null;
-		return node;
 	}
 	
+	private Node<E> removeMin(Node<E> node){
+		if(node == null) {
+			return null;
+		}
+		
+		if(node.left == null) { // 此时表示node为后继节点
+			Node<E> nodeRight = node.right;
+			node.right = null;
+			size--;
+			return nodeRight;
+		}
+		
+		node.left = removeMin(node.left);
+		return node;
+	}
+
+	public void preOrder(Node<E> node) {
+		if(node!=null) {
+			System.out.print(node.e+"->");
+			preOrder(node.left);
+			preOrder(node.right);
+		}
+	}
+
 	@Override
 	public String toString() {
 		return "BinaryTreeV2 [root=" + root + "]";
-	}
-
-	public static void main(String[] args) {
-		BaseTree<Integer> tree =  new BinaryTreeV2<>();
-		tree.insert(8);
-		tree.insert(9);
-		tree.insert(6);
-		tree.insert(7);
-		tree.insert(4);
-		tree.insert(3);
-		System.out.println(tree);
-		System.out.println(tree.remove(6));
-		System.out.println(tree);
 	}
 }
